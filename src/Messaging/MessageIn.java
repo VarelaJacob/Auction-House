2f3a15416a28259f7d4ebbf847b04bed1c224837
Jacob Varela <varelajacob@unm.edu>
2020-12-06 16:53:19 -0700

    Begin adding content to MessageIn class

--- src/Messaging/MessageIn.java
+++ src/Messaging/MessageIn.java
@@ -1,5 +1,31 @@
 package Messaging;
 
+import AuctionHouse.AuctionHouse;
+import Bank.Bank;
+import Messaging.MessageInfo;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class MessageIn {
+
+    private Socket socket;
+    private Bank bank;
+    private AuctionHouse auction;
+    private String source;
+    private String returnMessage;
+    private ObjectOutputStream outStream;
+    private ObjectInputStream  inputStream;
+ 
+    /*********** */
+    public MessageIn(Socket socket, Object reference) throws IOException {
+        
+        this.socket = socket;
+        setVar(reference);
+
+        outStream   = new ObjectOutputStream(socket.getOutputStream());
+        inputStream = new ObjectInputStream(socket.getInputStream()) ;
+    }
     
 }
