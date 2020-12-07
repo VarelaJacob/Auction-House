fc1d05af0c5f2adb39134f95832550cecc2422c6
Jacob Varela <varelajacob@unm.edu>
2020-12-07 01:40:33 -0700

    Added basic imports and variables.

--- src/Messaging/MessageOut.java
+++ src/Messaging/MessageOut.java
@@ -1,5 +1,24 @@
 package Messaging;
 
-public class MessageOut {
+import Agent.Agent;
+import AuctionHouse.AuctionHouse;
+import Messaging.MessageInfo;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MessageOut implements Runnable{
+    
+    public BlockingQueue<MessageInfo> outQueue;
+
+    public int bankPort;
+
+    private int portNum;
+
+    private String hostName;
     
 }
